322ca72959d2246f63963a38ac3f87e88dca050c
tabsman-afk <joseph.warren.enal@adamson.edu.ph>
2021-09-08 21:25:56 +0800

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+// -----------------Materials----------------------------
 import {MatButtonModule} from '@angular/material/button';
 import {MatBadgeModule} from '@angular/material/badge';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatMenuModule} from '@angular/material/menu';
-import {LoginformComponent} from './loginform/loginform.component';
-import { AppRoutingModule } from './app-routing.module';
 import {MatInputModule} from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { BookingformComponent } from './bookingform/bookingform.component';
 import {MatStepperModule} from '@angular/material/stepper';
-import { ReactiveFormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { AppliancesComponent } from './appliances/appliances.component';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSelectModule} from '@angular/material/select';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import { ProfileformComponent } from './profileform/profileform.component';
-import { MatNativeDateModule } from '@angular/material/core';
+import {MatNativeDateModule} from '@angular/material/core';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+// --------------Components-----------------------------
+import { HomepageComponent } from './homepage/homepage.component';
+import {LoginformComponent} from './loginform/loginform.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookingformComponent } from './bookingform/bookingform.component';
+import { AppliancesComponent } from './appliances/appliances.component';
+import { ProfileformComponent } from './profileform/profileform.component';
 import { ElectricalComponent } from './electrical/electrical.component';
 import { AircondetailsComponent } from './aircondetails/aircondetails.component';
 import { ServicedetailsComponent } from './servicedetails/servicedetails.component';
-import {MatTableModule} from '@angular/material/table';
 import { RequestdetailsComponent } from './requestdetails/requestdetails.component';
 import { RescheduleComponent } from './reschedule/reschedule.component';
 import { ConfirmcancelComponent } from './confirmcancel/confirmcancel.component';
@@ -37,11 +40,11 @@ import { CancelreasonsComponent } from './cancelreasons/cancelreasons.component'
 import { PaymentformComponent } from './paymentform/paymentform.component';
 import { AdminpageComponent } from './adminpage/adminpage.component';
 import { EmployeesComponent } from './employees/employees.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import { RefrigeratordetailsComponent } from './refrigeratordetails/refrigeratordetails.component';
 import { ElectricfandetailsComponent } from './electricfandetails/electricfandetails.component';
 import { WashingmachinedetailsComponent } from './washingmachinedetails/washingmachinedetails.component';
 import { HousecleaningComponent } from './housecleaning/housecleaning.component';
+import { GencleaningdetailsComponent } from './gencleaningdetails/gencleaningdetails.component';
 
 @NgModule({
   declarations: [
@@ -66,23 +69,25 @@ import { HousecleaningComponent } from './housecleaning/housecleaning.component'
     ElectricfandetailsComponent,
     WashingmachinedetailsComponent,
     HousecleaningComponent,
+    GencleaningdetailsComponent
 
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
     MatButtonModule,
     MatBadgeModule,
-    BrowserAnimationsModule,
     MatCardModule,
     MatMenuModule,
-    AppRoutingModule,
     MatInputModule,
-    FormsModule,
     MatIconModule,
     MatTabsModule,
     MatSidenavModule,
     MatStepperModule,
-    ReactiveFormsModule,
     MatFormFieldModule,
     MatSelectModule,
     MatDatepickerModule,
@@ -92,7 +97,6 @@ import { HousecleaningComponent } from './housecleaning/housecleaning.component'
     MatTableModule,
     MatToolbarModule,
     MatTableModule
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
